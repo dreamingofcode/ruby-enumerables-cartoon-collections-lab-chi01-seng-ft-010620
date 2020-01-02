0e5593dd611350c0b642741c4116070a5b09f3c6
cesar mota <cesarmota@ymail.com>
2020-01-02 21:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+#each_with_index
+  counter=0
+  while array.length > counter do
+  p "#{counter+ 1}.#{array[counter]}"
+  counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet (planeteer_calls)
+new_array=[]
+counter=0 
+while planeteer_calls.length> counter do
+  new_array.push(planeteer_calls[counter].capitalize)
+counter +=1 
+end
+ new_array.map! {|x|x+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array_of_words)
+  counter=0 
+  if array_of_words.length <=4
+    p false 
+  else
+  p true
+  end
 end
 
 def find_the_cheese# code an argument here
